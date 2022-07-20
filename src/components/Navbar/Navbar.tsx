bc6365fcbf04f1fcb8f28d56f8e3f7805536a1a0
GALw395 <galw395@mail.ru>
2022-07-20 19:55:20 +0300

    lesson 20

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
 
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}>
-                <a href="/profile">Profile</a>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink  to={"/profile"} className={({isActive}) =>`${isActive ? classes.active : ''}`}>Profile</NavLink>
             </div>
             <div className={`${classes.item} ${classes.active}`}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to={"/dialogs"} className={({isActive}) =>`${isActive ? classes.active : ''}`}>Messages</NavLink>
             </div>
-            <div className={classes.item}>
-                <a href="/news">News</a>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to={"/news"} className={({isActive}) =>`${isActive ? classes.active : ''}`}>News</NavLink>
             </div>
-            <div className={classes.item}>
-                <a href="/music">Music</a>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to={"/music"} className={({isActive}) =>`${isActive ? classes.active : ''}`}>Music</NavLink>
             </div>
-            <div className={classes.item}>
-                <a href="/settings">Settings</a>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to={"/settings"} className={({isActive}) =>`${isActive ? classes.active : ''}`}>Settings</NavLink>
             </div>
         </nav>
     )
