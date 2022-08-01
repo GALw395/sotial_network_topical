e48cd82b788c68bc64077f17a4712a2c65c6b93e
GALw395 <galw395@mail.ru>
2022-08-01 19:06:53 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,20 @@
-import classes from './Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className={classes.content} >
-            Dialogs
+        <div className={s.dialogs} >
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Alex</div>
+                <div className={s.dialog}>Lera</div>
+                <div className={s.dialog}>Tania</div>
+                <div className={s.dialog}>Marina</div>
+                <div className={s.dialog}>Nixie</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>Bla bla</div>
+                <div className={s.message}>Hi Nixie.</div>
+            </div>
         </div>
     )
 }
