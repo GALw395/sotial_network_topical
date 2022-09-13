96232b16c1d068ddeaf617e7c37bb9830a1c9d20
GALw395 <galw395@mail.ru>
2022-09-13 11:30:24 +0300

    lesson 28.1

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,35 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+
+let posts = [
+  { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+  { id: 2, message: 'My first post?', likesCount: 20 },
+  { id: 3, message: 'Bla bla bla', likesCount: 5 }
+]
+
+let dialogs = [
+  { id: 1, name: 'Alex' },
+  { id: 2, name: 'Lera' },
+  { id: 3, name: 'Tania' },
+  { id: 4, name: 'Marina' },
+  { id: 5, name: 'Nixie' }
+]
+
+let messages = [
+  { id: 1, message: 'Hi!' },
+  { id: 2, message: 'Bla bla' },
+  { id: 3, message: 'Hi Nixie.' }
+]
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App posts={posts} dialogs={dialogs} messages={messages} />
     </BrowserRouter>
   </React.StrictMode>
 );
